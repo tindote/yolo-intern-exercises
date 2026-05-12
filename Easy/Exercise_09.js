af59907473d1aa1a406ce94f23ac8a83f08f0fd3
tindote <tinbon2406@gmail.com>
2026-05-12 15:22:56 +0700

    Exercise_09: Tinh so du phep chia a cho b

---
+++ Easy/Exercise_09.js
@@ -0,0 +1,17 @@
+function getRemainder(a,b){
+    if (b == 0){
+        return "Khong the chia cho 0";
+    }
+    var res = a/b;
+    if (res>-1 && res<1){
+            return a;
+    }else{
+        if(res<0){
+            return a - (b * Math.ceil(res));
+        }
+        return a - (b * Math.floor(res));
+    }
+}
+
+var a = -80, b = 90
+console.log(getRemainder(a,b))

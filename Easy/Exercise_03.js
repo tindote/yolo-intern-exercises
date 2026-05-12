d2abf18a7d9c81966441d40d343df4b5c2bda253
tindote <tinbon2406@gmail.com>
2026-05-12 14:09:45 +0700

    Hoan thanh Exercise-03: Kiem tra tinh dong chat chan le

---
+++ Easy/Exercise_03.js
@@ -0,0 +1,19 @@
+function test(a,b){
+    let count = 0;
+    if(a%2==0){
+        count++;
+    }
+    if(b%2==0){
+        count++;
+    }
+    return count;
+}
+
+var count = test(10, 21);
+if (count ==2){
+    console.log("Cung chan!");
+}else if(count == 1){
+    console.log("1 chan, 1 le");
+}else{
+    console.log("Cung le");
+}

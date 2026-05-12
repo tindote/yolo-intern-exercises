77794ec61d732eedc90c9baefddbe4150575c0c1
tindote <tinbon2406@gmail.com>
2026-05-12 15:37:02 +0700

    Exercise_12: Calculate average of array

---
+++ Easy/Exercise_12.js
@@ -0,0 +1,13 @@
+function findAverage(array) {
+    if (array.length === 0) {
+        return 0; 
+    }
+    var sum = 0;
+    for (var i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum / array.length;
+}
+
+var data = [1, 3, 6, 3, 7, 9, 8, 0, 11, 32, 43];
+console.log(findAverage(data));

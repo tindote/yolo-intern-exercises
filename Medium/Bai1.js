8fb3617b460cda5e3840ddfae774a5c7d627ef42
tindote <tinbon2406@gmail.com>
2026-05-12 16:19:15 +0700

    Bai1: Sort array by string

---
+++ Medium/Bai1.js
@@ -0,0 +1,12 @@
+function sortArrayByString(arr, str) {
+    if(str === "Tang") {
+        return arr.sort((a, b) => a - b);
+    }else if(str === "Giam") {
+        return arr.sort((a, b) => b - a);
+    }   
+}
+
+// Example usage:
+const arr = [5, 2, 9, 1, 5, 6];
+console.log(sortArrayByString(arr, "Tang"));
+console.log(sortArrayByString(arr, "Giam"));

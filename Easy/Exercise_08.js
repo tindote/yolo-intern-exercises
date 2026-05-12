859303f424431be5f8786dd21dec6e16c87a47f3
tindote <tinbon2406@gmail.com>
2026-05-12 14:45:03 +0700

    Exercise_08:Kiem tra so nguyen to

---
+++ Easy/Exercise_08.js
@@ -0,0 +1,9 @@
+function checkPrime(a){
+    for(let i = 2; i< Math.sqrt(a);i++){
+        if(a%i==0){
+            return "khong";
+        }
+    }
+    return "co";
+}
+console.log(checkPrime(13));

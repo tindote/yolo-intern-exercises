11d66b8a2873687d2947283d95a14195c8ef4897
tindote <tinbon2406@gmail.com>
2026-05-12 17:54:39 +0700

    Bai 13, 14, 15

---
+++ Medium/Bai13.js
@@ -0,0 +1,16 @@
+function calculateTax(incomes) {
+    var totalTax = 0;
+    for (var i = 0; i < incomes.length; i++) {
+        var income = incomes[i];
+        if (income <= 20) {
+            totalTax += income * 0.05;
+        } else if (income <= 50) {
+            totalTax += income * 0.1;
+        } else {
+            totalTax += income * 0.2;
+        }
+    }
+    return totalTax;
+}
+var incomes = [10, 30, 60];
+console.log(calculateTax(incomes));

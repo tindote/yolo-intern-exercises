99596c6964bd95cf0b5d1797c95337eb2f9f1558
tindote <tinbon2406@gmail.com>
2026-05-13 23:49:37 +0700

    Update rename and few remained exercises

--- Medium/Bai14.js
+++ Medium/Bai14.js
@@ -1,9 +1,14 @@
-function sumDigits(n) {
-    let sum = 0;
-    while (n > 0) {
-        sum += n % 10;
-        n = Math.floor(n / 10);
+function countElements(arrA, arrB) {
+    let count = 0;
+    for (let i = 0; i < arrA.length; i++) {
+        for (let j = 0; j < arrB.length; j++) {
+            if (arrA[i] === arrB[j]) {
+                count++;
+            }
+        }
     }
-    return sum;
-}   
-console.log(sumDigits(123));
+    return arrA.length-count;
+}
+const arrA = [1, 2, 3, 4];
+const arrB = [3, 4, 5, 6];
+console.log(countElements(arrA, arrB)); 
